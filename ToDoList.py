363f7543d42531b6bd2637ca623f1e3fc68d1fe2
LiMaCor <julian.itsa8@gmail.com>
2023-02-25 00:37:13 +0100

    Added function to define initial position for the app using geometry method

--- ToDoList.py
+++ ToDoList.py
@@ -1,11 +1,21 @@
 import tkinter as tk
 
+# setInitialPosition -> Defines the initial position for an app passed as parameter
+def setInitialPosition(appToSetToCenter):
+    # Set coordinates
+    xAxis = appToSetToCenter.winfo_screenwidth() // 2
+    yAxis = int(appToSetToCenter.winfo_screenheight() * 0.1)
+    
+    # Set app position
+    appToSetToCenter.geometry('500x600+' + str(xAxis) + '+' + str(yAxis))
+
 # Init app
 root = tk.Tk()
 
 # App settings
 root.title("Bardo app")
-root.eval("tk::PlaceWindow . center")
+#root.eval("tk::PlaceWindow . center")
+setInitialPosition(appToSetToCenter = root)
 
 # Initial app structure settings
 initialFrame = tk.Frame(root, width=500, height=600, bg="#EC00FF")
