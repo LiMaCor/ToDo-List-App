8a490b4a1355c95c7be2a9ec53a30af24f2cb5cd
LiMaCor <julian.itsa8@gmail.com>
2023-02-25 00:06:25 +0100

    Relocated app settings. Setted initial app structure settings

--- ToDoList.py
+++ ToDoList.py
@@ -2,8 +2,14 @@ import tkinter as tk
 
 # Init app
 root = tk.Tk()
+
+# App settings
 root.title("Bardo app")
 root.eval("tk::PlaceWindow . center")
+
+# Initial app structure settings
+initialFrame = tk.Frame(root, width=500, height=600, bg="#EC00FF")
+initialFrame.grid(row=0, column=0)
 
 # Run app
 root.mainloop()
